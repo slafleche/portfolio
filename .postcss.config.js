ad09d46ced47c5ef07338cc46c682c0a26701e7c
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2025-09-04 14:32:07 -0400

    Fixed duplicate postcss config fixed package scripts renamed styles var

--- .postcss.config.js
+++ .postcss.config.js
@@ -1,20 +1,5 @@
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2022: true,
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: { jsx: true },
-  },
-  extends: ["standard", "plugin:import/recommended"],
-  plugins: ["@typescript-eslint"],
-  rules: {
-    // example rules you can customize
-    "no-unused-vars": "warn",
-    semi: ["error", "always"],
-  },
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [autoprefixer()],
 };
