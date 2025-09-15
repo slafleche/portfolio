de997582835100e5dbfa2a3cda3cb61a38f64eb6
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2025-09-15 11:12:41 -0400

    Adding menu component to page Wired up arch helper to arch component Refactored helper arch function Added arch preview page for reference/testing

--- src/components/Arch.tsx
+++ src/components/Arch.tsx
@@ -3,25 +3,32 @@ import clsx from 'clsx';
 import type { ReactNode } from 'react';
 import * as s from '@/styles/components/arch.css';
 import { useWindowSize } from '@/lib/responsive/WindowSizeContext';
-import { IArch } from '../lib/arch/archHelper';
+import { generateArchPath, IArch } from '../lib/arch/archHelper';
 
 type Props = IArch & {
+  width: number;
   className?: string;
   children: ReactNode;
 };
 
-export default function Arch({
-  containerHeight = 40,
-  curveHeight = 16,
-  maskOffset = 70,
-  bumpHeight = 12,
-  bumpWidth = 50,
-  bumpRoundness = 0.85,
-  bumpSpan = 7,
-  className,
-  children,
-}: Props) {
-  const { width } = useWindowSize();
+export default function Arch({ className, children }: Props) {
+  const windowSize = useWindowSize().width;
+  if (!windowSize) return null; // wait to render if we don't have the window size
+
+  const archProps: IArch = {
+    top: 40,
+    curveHeight: 16,
+    ry: 70,
+    bumpHeight: 12,
+    bumpWidth: 50,
+    bumpBaseWidth: 0.85,
+    bumpTipWidth: 7,
+  };
+
+  const d = generateArchPath({
+    ...archProps,
+    width: windowSize,
+  });
 
   return (
     <div className={clsx(className, s.arch)}>
@@ -29,23 +36,11 @@ export default function Arch({
         className={s.svg}
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 ${width} ${height}"
-        width="${width}"
-        height="${height}"
+        width={windowSize}
+        height={archProps.top + archProps.curveHeight}
         preserveAspectRatio="none"
       >
-        <path
-          d={buildNavSVG({
-            width,
-            top: containerHeight,
-            curve: curveHeight,
-            ry: maskOffset,
-            bulgeDepth: bumpHeight,
-            bulgeWidth: bumpWidth,
-            shoulder: bumpRoundness,
-            tipRound: bumpSpan,
-          })}
-          fill="currentColor"
-        />
+        <path d={d} fill="currentColor" />
       </svg>
       {children}
     </div>
