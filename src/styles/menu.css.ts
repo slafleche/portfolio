b2dfc2e6483a04fc61971bd617b1d85178a54e5e
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2025-09-22 22:52:59 -0400

    Don't render Bokeh until window data comes in Adjusted menu animation

--- src/styles/menu.css.ts
+++ src/styles/menu.css.ts
@@ -9,7 +9,7 @@ export const menu = style({
   width: '100%',
   zIndex: 100,
   transform: `translate3d(0, -${(archVars.top + archVars.curveHeight) * 1.1}px, 0)`,
-  transition: 'transform 0.7s cubic-bezier(0.22, 1, 0.36, 1) 0.2s',
+  transition: 'transform 0.8s cubic-bezier(0.69, 0.42, 0.01, 1) 0.2s',
   willChange: 'transform',
   backfaceVisibility: 'hidden',
   contain: 'layout paint',
