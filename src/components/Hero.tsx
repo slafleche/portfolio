8245f378d9c529ad14937c52793326f4cdcc42aa
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2025-10-02 22:13:13 -0400

    Updated Hero to render markdown

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,9 +6,15 @@ import * as layoutStyles from '@/styles/layout.css';
 import SkipToContent from './SkipToContent';
 import ImageByName from './ImageByName';
 import GlassyPanel from './GlassyPanel';
+import ReactMarkdown from 'react-markdown';
+import type { Components } from 'react-markdown';
 
 type Props = {
 	className?: string;
+};
+
+const inlineMarkdownComponents: Components = {
+	p: ({ children }) => <>{children}</>,
 };
 
 export default function Hero({ className }: Props) {
@@ -26,8 +32,16 @@ export default function Hero({ className }: Props) {
 			{/* <div className={s.gradient} aria-hidden /> */}
 			<div className={clsx(layoutStyles.content, s.content)}>
 				<GlassyPanel surfaceClassName={clsx(layoutStyles.panel, s.panel)}>
-					<h1 className={s.heading}>{t('hero')}</h1>
-					<p className={s.paragraph}>{t('hero-subtitle')}</p>
+					<h1 className={s.heading}>
+						<ReactMarkdown components={inlineMarkdownComponents}>
+							{t('hero')}
+						</ReactMarkdown>
+					</h1>
+					<p className={s.paragraph}>
+						<ReactMarkdown components={inlineMarkdownComponents}>
+							{t('hero-subtitle')}
+						</ReactMarkdown>
+					</p>
 				</GlassyPanel>
 			</div>
 
