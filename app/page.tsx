930207171b5ad349d027c76b284f42a638491a27
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2025-10-20 12:52:25 -0400

    Added async to RootPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 // This page is only to redirect from `/` to `/{locale}`
 import { headers } from 'next/headers';
-import type { ReadonlyHeaders } from 'next/dist/server/web/spec-extension/adapters/headers';
 import { redirect } from 'next/navigation';
 import {
 	DEFAULT_LOCALE,
@@ -8,8 +7,8 @@ import {
 } from '@/lib/locales/locale';
 import type { Locale } from '@/data/locales';
 
-export default function RootPage() {
-	const headerList: ReadonlyHeaders = headers();
+export default async function RootPage() {
+	const headerList = await headers();
 	const accept = headerList.get('accept-language');
 	const target: Locale =
 		pickLocaleFromAcceptLanguage(accept) ?? DEFAULT_LOCALE;
