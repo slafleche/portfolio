b3d3ad00a41575a417a8e9bdc3e720b8440a9855
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2025-11-03 15:47:24 -0500

    Declare all css unit types with helper wrapper and export type

--- src/styles/measurement/index.ts
+++ src/styles/measurement/index.ts
@@ -182,8 +182,196 @@ const createMeasurement = (
 export const m = (value: number, unit: string = 'px'): IMeasurement =>
   createMeasurement(value, unit.toLowerCase());
 
-export const mPercent = (value: number): IMeasurement =>
-  createMeasurement(value, '%');
+type UnitHelper<Unit extends string = string> = ((value: number) => IMeasurement) & {
+  unit: Unit;
+};
+
+const makeUnitHelper = <Unit extends string>(unit: Unit): UnitHelper<Unit> => {
+  const normalizedUnit = unit.toLowerCase() as Unit;
+  const helper = ((value: number) =>
+    createMeasurement(value, normalizedUnit)) as UnitHelper<Unit>;
+  helper.unit = normalizedUnit;
+  return helper;
+};
+
+export const mPercent = makeUnitHelper('%');
+
+export const mPx = makeUnitHelper('px');
+export const mCm = makeUnitHelper('cm');
+export const mMm = makeUnitHelper('mm');
+export const mQ = makeUnitHelper('q');
+export const mIn = makeUnitHelper('in');
+export const mPc = makeUnitHelper('pc');
+export const mPt = makeUnitHelper('pt');
+
+export const mEm = makeUnitHelper('em');
+export const mRem = makeUnitHelper('rem');
+export const mEx = makeUnitHelper('ex');
+export const mRex = makeUnitHelper('rex');
+export const mCh = makeUnitHelper('ch');
+export const mRch = makeUnitHelper('rch');
+export const mCap = makeUnitHelper('cap');
+export const mRcap = makeUnitHelper('rcap');
+export const mIc = makeUnitHelper('ic');
+export const mRic = makeUnitHelper('ric');
+export const mLh = makeUnitHelper('lh');
+export const mRlh = makeUnitHelper('rlh');
+
+export const mVw = makeUnitHelper('vw');
+export const mVh = makeUnitHelper('vh');
+export const mVi = makeUnitHelper('vi');
+export const mVb = makeUnitHelper('vb');
+export const mVmin = makeUnitHelper('vmin');
+export const mVmax = makeUnitHelper('vmax');
+
+export const mSvw = makeUnitHelper('svw');
+export const mSvh = makeUnitHelper('svh');
+export const mSvi = makeUnitHelper('svi');
+export const mSvb = makeUnitHelper('svb');
+export const mSvmin = makeUnitHelper('svmin');
+export const mSvmax = makeUnitHelper('svmax');
+
+export const mLvw = makeUnitHelper('lvw');
+export const mLvh = makeUnitHelper('lvh');
+export const mLvi = makeUnitHelper('lvi');
+export const mLvb = makeUnitHelper('lvb');
+export const mLvmin = makeUnitHelper('lvmin');
+export const mLvmax = makeUnitHelper('lvmax');
+
+export const mDvw = makeUnitHelper('dvw');
+export const mDvh = makeUnitHelper('dvh');
+export const mDvi = makeUnitHelper('dvi');
+export const mDvb = makeUnitHelper('dvb');
+export const mDvmin = makeUnitHelper('dvmin');
+export const mDvmax = makeUnitHelper('dvmax');
+
+export const mCqw = makeUnitHelper('cqw');
+export const mCqh = makeUnitHelper('cqh');
+export const mCqi = makeUnitHelper('cqi');
+export const mCqb = makeUnitHelper('cqb');
+export const mCqmin = makeUnitHelper('cqmin');
+export const mCqmax = makeUnitHelper('cqmax');
+
+export const mDeg = makeUnitHelper('deg');
+export const mRad = makeUnitHelper('rad');
+export const mGrad = makeUnitHelper('grad');
+export const mTurn = makeUnitHelper('turn');
+
+export const mS = makeUnitHelper('s');
+export const mMs = makeUnitHelper('ms');
+
+export const mHz = makeUnitHelper('hz');
+export const mKhz = makeUnitHelper('khz');
+
+export const mDpi = makeUnitHelper('dpi');
+export const mDpcm = makeUnitHelper('dpcm');
+export const mDppx = makeUnitHelper('dppx');
+
+export const mFr = makeUnitHelper('fr');
+
+type MeasurementOf<T extends UnitHelper> = ReturnType<T>;
+
+type UnitGuard<T extends UnitHelper> = (value: unknown) => value is MeasurementOf<T>;
+
+type UnitAssertion<T extends UnitHelper> = (
+  value: unknown,
+  context?: string,
+) => asserts value is MeasurementOf<T>;
+
+const makeUnitGuard = <T extends UnitHelper>(helper: T): UnitGuard<T> => {
+  return (value: unknown): value is MeasurementOf<T> =>
+    isMeasurement(value) && value.isUnit(helper.unit);
+};
+
+const makeUnitAssert = <T extends UnitHelper>(helper: T): UnitAssertion<T> => {
+  const guard = makeUnitGuard(helper);
+  return (value: unknown, context?: string): asserts value is MeasurementOf<T> => {
+    if (!guard(value)) {
+      const location = context ? `${context}: ` : '';
+      throw new Error(
+        `${location}Expected measurement with unit "${helper.unit}".`,
+      );
+    }
+  };
+};
+
+export type PercentMeasurement = MeasurementOf<typeof mPercent>;
+
+export type PxMeasurement = MeasurementOf<typeof mPx>;
+export type CmMeasurement = MeasurementOf<typeof mCm>;
+export type MmMeasurement = MeasurementOf<typeof mMm>;
+export type QMeasurement = MeasurementOf<typeof mQ>;
+export type InMeasurement = MeasurementOf<typeof mIn>;
+export type PcMeasurement = MeasurementOf<typeof mPc>;
+export type PtMeasurement = MeasurementOf<typeof mPt>;
+
+export type EmMeasurement = MeasurementOf<typeof mEm>;
+export type RemMeasurement = MeasurementOf<typeof mRem>;
+export type ExMeasurement = MeasurementOf<typeof mEx>;
+export type RexMeasurement = MeasurementOf<typeof mRex>;
+export type ChMeasurement = MeasurementOf<typeof mCh>;
+export type RchMeasurement = MeasurementOf<typeof mRch>;
+export type CapMeasurement = MeasurementOf<typeof mCap>;
+export type RcapMeasurement = MeasurementOf<typeof mRcap>;
+export type IcMeasurement = MeasurementOf<typeof mIc>;
+export type RicMeasurement = MeasurementOf<typeof mRic>;
+export type LhMeasurement = MeasurementOf<typeof mLh>;
+export type RlhMeasurement = MeasurementOf<typeof mRlh>;
+
+export type VwMeasurement = MeasurementOf<typeof mVw>;
+export type VhMeasurement = MeasurementOf<typeof mVh>;
+export type ViMeasurement = MeasurementOf<typeof mVi>;
+export type VbMeasurement = MeasurementOf<typeof mVb>;
+export type VminMeasurement = MeasurementOf<typeof mVmin>;
+export type VmaxMeasurement = MeasurementOf<typeof mVmax>;
+
+export type SvwMeasurement = MeasurementOf<typeof mSvw>;
+export type SvhMeasurement = MeasurementOf<typeof mSvh>;
+export type SviMeasurement = MeasurementOf<typeof mSvi>;
+export type SvbMeasurement = MeasurementOf<typeof mSvb>;
+export type SvminMeasurement = MeasurementOf<typeof mSvmin>;
+export type SvmaxMeasurement = MeasurementOf<typeof mSvmax>;
+
+export type LvwMeasurement = MeasurementOf<typeof mLvw>;
+export type LvhMeasurement = MeasurementOf<typeof mLvh>;
+export type LviMeasurement = MeasurementOf<typeof mLvi>;
+export type LvbMeasurement = MeasurementOf<typeof mLvb>;
+export type LvminMeasurement = MeasurementOf<typeof mLvmin>;
+export type LvmaxMeasurement = MeasurementOf<typeof mLvmax>;
+
+export type DvwMeasurement = MeasurementOf<typeof mDvw>;
+export type DvhMeasurement = MeasurementOf<typeof mDvh>;
+export type DviMeasurement = MeasurementOf<typeof mDvi>;
+export type DvbMeasurement = MeasurementOf<typeof mDvb>;
+export type DvminMeasurement = MeasurementOf<typeof mDvmin>;
+export type DvmaxMeasurement = MeasurementOf<typeof mDvmax>;
+
+export type CqwMeasurement = MeasurementOf<typeof mCqw>;
+export type CqhMeasurement = MeasurementOf<typeof mCqh>;
+export type CqiMeasurement = MeasurementOf<typeof mCqi>;
+export type CqbMeasurement = MeasurementOf<typeof mCqb>;
+export type CqminMeasurement = MeasurementOf<typeof mCqmin>;
+export type CqmaxMeasurement = MeasurementOf<typeof mCqmax>;
+
+export type DegMeasurement = MeasurementOf<typeof mDeg>;
+export type RadMeasurement = MeasurementOf<typeof mRad>;
+export type GradMeasurement = MeasurementOf<typeof mGrad>;
+export type TurnMeasurement = MeasurementOf<typeof mTurn>;
+
+export type SMeasurement = MeasurementOf<typeof mS>;
+export type MsMeasurement = MeasurementOf<typeof mMs>;
+
+export type HzMeasurement = MeasurementOf<typeof mHz>;
+export type KhzMeasurement = MeasurementOf<typeof mKhz>;
+
+export type DpiMeasurement = MeasurementOf<typeof mDpi>;
+export type DpcmMeasurement = MeasurementOf<typeof mDpcm>;
+export type DppxMeasurement = MeasurementOf<typeof mDppx>;
+
+export type FrMeasurement = MeasurementOf<typeof mFr>;
+
+export const isPercentMeasurement = makeUnitGuard(mPercent);
+export const assertPercentMeasurement = makeUnitAssert(mPercent);
 
 export const double = (measurement: IMeasurement) =>
   measurement.double();
