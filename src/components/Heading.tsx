f6ba630107bd3ff9f399a7b4df8e30352bd75dbd
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2025-11-04 14:46:24 -0500

    pass data props through Cards and Heading components

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,27 +1,31 @@
-import type { ReactNode } from 'react';
+import type {
+	ComponentPropsWithoutRef,
+	ReactNode,
+} from 'react';
 
 export interface IHeadingDepth {
 	depth?: 2 | 3 | 4 | 5 | 6;
 }
 
-type Props = IHeadingDepth & {
-	className?: string;
-	children: ReactNode;
-	id?: string;
-};
+type HeadingProps = IHeadingDepth &
+	ComponentPropsWithoutRef<'h2'> & {
+		children: ReactNode;
+	};
 
 export default function Heading({
 	depth = 3,
 	className,
 	children,
 	id,
-}: Props) {
+	...rest
+}: HeadingProps) {
 	const Tag = `h${depth || 2}` as 'h2';
 	return (
 		<Tag
 			id={id}
 			className={className}
 			data-ui="heading"
+			{...rest}
 		>
 			{children}
 		</Tag>
