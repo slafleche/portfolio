a622479c771d35d7e988f487837e54e5d0cfb1be
Stéphane LaFlèche <19651440+slafleche@users.noreply.github.com>
2025-11-11 07:41:18 -0500

    Added vitest unit tests with some basic testing for MK

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config';
+import path from 'node:path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['tests/**/*.test.ts'],
+    coverage: {
+      enabled: false,
+    },
+  },
+});
